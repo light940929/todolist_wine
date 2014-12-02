83d6fddf3f459012245163f91b30a1983da6081f
Hannah Lin <light940929@gmail.com>
2014-12-02 17:07:32 +0800

    check install path

--- environment/lamp/installLAMP.sh
+++ environment/lamp/installLAMP.sh
@@ -38,9 +38,9 @@ rm -f $tfile
 /usr/local/bin/supervisord -n
 
 # install todolist_wine
-git config --global user.name "Hannah Lin"
-git config --global user.email "light940929@gmail.com"
-git clone https://github.com/light940929/todolist_wine.git
+#git config --global user.name "Hannah Lin"
+#git config --global user.email "light940929@gmail.com"
+#git clone https://github.com/light940929/todolist_wine.git
 
 # install todolist_wine DB
 mysql -r root todolist_wine < cd ~/todolist_wine/db/todolist_wine1127.sql
