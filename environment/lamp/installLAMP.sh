8fd9b06888da4dc850adcce138874b27537e0fdc
Hannah Lin <light940929@gmail.com>
2014-12-02 14:40:57 +0800

    install todolist_wine db

--- environment/lamp/installLAMP.sh
+++ environment/lamp/installLAMP.sh
@@ -1,17 +1,4 @@
 #!bin/bash
-# install docker
-
-apt-get update
-apt-get install docker.io
-
-apt-get install linux-image-extra-`uname -r` -y
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-
-apt-get install lxc-docker -y
-groupadd docker
-gpasswd -a ${USER} docker
-service docker restart
 
 # install LAMP
 set -e
@@ -49,6 +36,13 @@ rm -f $tfile
 
 # start all the services
 /usr/local/bin/supervisord -n
+
+# install todolist_wine DB
+mysql -r root todolist_wine < cd ~/todolist_wine/db/todolist_wine1127.sql
+
+
+
+
 
 
 
