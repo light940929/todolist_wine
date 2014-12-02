f97d85b84efe0eb69dc57349b9c34a3637d2ea27
Hannah Lin <light940929@gmail.com>
2014-12-02 14:39:22 +0800

    install Docker on Ubuntu

---
+++ environment/docker/installDocker.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+# install docker
+
+apt-get update
+apt-get -y install docker.io
+
+ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
+
+update-rc.d docker.io defaults
+
+docker restart
